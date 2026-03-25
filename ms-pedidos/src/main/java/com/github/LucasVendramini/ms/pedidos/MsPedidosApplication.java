6df05f8beb57baa2c62ef754c47e3ff073a4b17b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 20:05:06 -0300

    Enviando ms-pedidos

---
+++ ms-pedidos/src/main/java/com/github/LucasVendramini/ms/pedidos/MsPedidosApplication.java
@@ -0,0 +1,16 @@
+package com.github.LucasVendramini.ms.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPedidosApplication.class, args);
+
+		
+
+	}
+
+}
