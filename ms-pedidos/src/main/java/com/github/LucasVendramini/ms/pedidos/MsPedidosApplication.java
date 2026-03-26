a47fef0a42be2badfc50814b69df333638dab21f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 21:55:03 -0300

    Adicionando entidades e carga DB

--- ms-pedidos/src/main/java/com/github/LucasVendramini/ms/pedidos/MsPedidosApplication.java
+++ ms-pedidos/src/main/java/com/github/LucasVendramini/ms/pedidos/MsPedidosApplication.java
@@ -9,7 +9,7 @@ public class MsPedidosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsPedidosApplication.class, args);
 
-		
+
 
 	}
 
---
+++ ms-pedidos/src/main/java/com/github/LucasVendramini/ms/pedidos/entities/Pedido.java
@@ -0,0 +1,45 @@
+package com.github.LucasVendramini.ms.pedidos.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //@Column define as características da coluna no Banco de Dados
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+    //@Column(unique = true (pode fazer ou não), nullable = false, lengh = 11
+    @Column(nullable = false, length = 11)
+    private String cpf;
+    //A data ao definir um pedido vai ser a que estiver no sistema naquele momento
+    private LocalDate data;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    //Valor calculado
+    private BigDecimal valorTotal;
+
+    //Relacionamento
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<ItemDoPedido> itens = new ArrayList<>();
+
+    public void calcularValorTotalPedido() {
+        this.valorTotal = this.itens.stream().map(i -> i.getPrecoUnitario()
+                .multiply(BigDecimal.valueOf(i.getQuantidade())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
